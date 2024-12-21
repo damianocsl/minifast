3821a6b92b8d72cb9773869bfb34faed08bce891
damianocsl <133384087+damianocsl@users.noreply.github.com>
2024-12-21 15:13:14 +0100

    update: adjust header spacing

--- code/Header.tsx
+++ code/Header.tsx
@@ -5,9 +5,9 @@ import { ToggleThemeButton } from './ToggleThemeButton'
 
 export function Header() {
   return (
-    <XStack gap="$4" w="100%" px={8} py={4} jc="space-between" ai="center">
+    <XStack gap="$4" w="100%" px={16} py={8} jc="space-between" ai="center">
       <Image src={appIcon} w={BRANDING_SIZE} h={BRANDING_SIZE} />
-      <Text fs={BRANDING_SIZE}>Minifast</Text>
+      <Text fontSize={BRANDING_SIZE}>Minifast</Text>
       <ToggleThemeButton />
     </XStack>
   )
